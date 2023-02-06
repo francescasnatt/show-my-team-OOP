6a60a68e6581668bbf56c987950f3630cfcdd3c2
Francesca Snatt <francesca.snatt@outlook.com>
2023-02-06 21:32:46 +0000

    finished project and added gitignore

--- starter/index.js
+++ starter/index.js
@@ -5,6 +5,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const teamMembers = [];
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -13,7 +15,7 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const questions = [
+const managerQs = [
     {
       type: 'input',
       message: "What is your team manager's name?",
@@ -42,19 +44,123 @@ const questions = [
     },
   ];
 
+
+  const engineerQs = [
+    {
+      type: 'input',
+      message: "What is your engineer's name?",
+      name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's id?",
+        name: 'engineerId',
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's email?",
+        name: 'engineerEmail',
+    },
+    {
+        type: 'input',
+        message: "What is your engineer's github username?",
+        name: 'engineerGithub',
+    },
+    {
+    type: 'list',
+    message: "Which type of team member would you like to add?",
+    name: 'newMember',
+    choices: ['Engineer', 'Intern', 'None'],
+    },
+  ];
+
+
+  const internQs = [
+    {
+      type: 'input',
+      message: "What is your intern's name?",
+      name: 'internName',
+    },
+    {
+        type: 'input',
+        message: "What is your intern's id?",
+        name: 'internId',
+    },
+    {
+        type: 'input',
+        message: "What is your intern's email?",
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: "What is your intern's school called?",
+        name: 'internSchool',
+    },
+    {
+    type: 'list',
+    message: "Which type of team member would you like to add?",
+    name: 'newMember',
+    choices: ['Engineer', 'Intern', 'None'],
+    },
+  ];
+
   // function to render info to page
-function render() {
-    
-  }
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+  {err ? console.error(err) : console.log("Success!")});
+}
   
   // function to initialize program
-  function init() {
+  function managerPrompt() {
     inquirer
-    .prompt(questions)
+    .prompt(managerQs)
     .then((response) => {
-      render()
+        const member = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficenum);
+        teamMembers.push(member);
+        if(response.newMember === "Engineer"){
+            engineerPrompt();
+        } else if(response.newMember === "Intern"){
+            internPrompt();
+        } else {
+        const teamInfo = render(teamMembers);
+        writeToFile('index.html', teamInfo);
+        }
+      });
+  }
+
+  function engineerPrompt() {
+    inquirer
+    .prompt(engineerQs)
+    .then((response) => {
+        const member = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
+        teamMembers.push(member);
+        if(response.newMember === "Engineer"){
+            engineerPrompt();
+        } else if(response.newMember === "Intern"){
+            internPrompt();
+        } else {
+        const teamInfo = render(teamMembers);
+        writeToFile('index.html', teamInfo);
+        }
+      });
+  }
+
+  function internPrompt() {
+    inquirer
+    .prompt(internQs)
+    .then((response) => {
+        const member = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
+        teamMembers.push(member);
+        if(response.newMember === "Engineer"){
+            engineerPrompt();
+        } else if(response.newMember === "Intern"){
+            internPrompt();
+        } else {
+        const teamInfo = render(teamMembers);
+        writeToFile('index.html', teamInfo);
+        }
       });
   }
   
   // function call to initialize program
-  init();
+  managerPrompt();
