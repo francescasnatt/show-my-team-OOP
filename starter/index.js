569c18203824856df1aeff1c84905e148b845df7
Francesca Snatt <francesca.snatt@outlook.com>
2023-02-06 18:13:42 +0000

    added manager prompts

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,48 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const questions = [
+    {
+      type: 'input',
+      message: "What is your team manager's name?",
+      name: 'managerName',
+    },
+    {
+        type: 'input',
+        message: "What is your team manager's id?",
+        name: 'managerId',
+    },
+    {
+        type: 'input',
+        message: "What is your team manager's email?",
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: "What is your team manager's office number?",
+        name: 'managerOfficenum',
+    },
+    {
+    type: 'list',
+    message: "Which type of team member would you like to add?",
+    name: 'newMember',
+    choices: ['Engineer', 'Intern', 'None'],
+    },
+  ];
+
+  // function to render info to page
+function render() {
+    
+  }
+  
+  // function to initialize program
+  function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+      render()
+      });
+  }
+  
+  // function call to initialize program
+  init();
